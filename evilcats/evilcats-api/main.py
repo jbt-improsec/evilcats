a47d3840f4965777ac52a50b94f0ebb337833961
jakobbnt <jakob.toersloev@gmail.com>
2023-03-23 13:05:56 +0100

    Dockerimage is now buildable

--- evilcats/evilcats-api/main.py
+++ evilcats/evilcats-api/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request, UploadFile, HTTPException, status, Query, Form
+from fastapi import FastAPI, Response, Request, UploadFile, HTTPException, status, Query, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from zipfile import ZipFile
 from typing import List, Annotated
+from PIL import Image
+import datetime
 import os
 
 #python -m uvicorn main:app --reload 
@@ -26,10 +28,14 @@ async def catpic(request: Request):
 @app.post("/catpics/addpics/")
 async def add_cat_pic(file: UploadFile):
     try:
-        zip = ZipFile(file.file)
-        for sub_file in zip.namelist():
-            if sub_file.endswith('.jpg') or sub_file.endswith('.jpeg') or sub_file.endswith('.png'):
-                zip.extract(sub_file,'./static/')
+        if file.filename.endswith('.jpg') or file.filename.endswith('.jpeg') or file.filename.endswith('.png'):
+            img = Image.open(file.file)
+            img.save('./static/cats/{timestamp}-{filename}'.format(filename=file.filename,timestamp=datetime.datetime.now().microsecond))
+        else:
+            zip = ZipFile(file.file)
+            for sub_file in zip.namelist():
+                if sub_file.endswith('.jpg') or sub_file.endswith('.jpeg') or sub_file.endswith('.png'):
+                    zip.extract(sub_file,'./static/')
     except Exception as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="Item not found")
